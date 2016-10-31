5ce9c37822f1b7d5188c73d666969832f67f5f7d
annave123 <tony.nave@aol.com>
2016-10-30 20:18:21 -1000

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+
+//Author: Charles Cook
+//Date created: Mar 22, 2005
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here sifnes
+        System.out.println("From: Bill Smith");
+        System.out.println("Dell Computer, Bldg 13");
+        System.out.println("April 12, 2005");
+        System.out.println("");
+        System.out.println("Jack Jones");
+        System.out.println("");
+        System.out.println("Message: Help! I'm trapped inside a computer!");
+
     }
 }
